c5b78758da17ad87a42fe14a33c51637d2565001
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-06 14:46:24 -0700

    Added some references to alphabets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+const transliterationAlphabets = ['Baybayin', 'Aurebesh', 'Deseret']
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const alphabet = transliterationAlphabets[getRandomInt(2)]
+
+  return (
+    <header className="App-header">
+      <h1>The Baybayin Project</h1>
+      <h2>What is {alphabet}</h2>
+      <p>{alphabet} Transliterator</p>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main>
+        <h2> What is Baybayin? </h2>
+      </main>
     </div>
   );
 }
