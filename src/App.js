d144f733df9a51b549102aefdca009c61463e606
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-07 15:30:44 -0700

    re-added CoreConcept example

--- src/App.js
+++ src/App.js
@@ -67,9 +67,9 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
           <CoreConcept 
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
           />
           <CoreConcept {...CORE_CONCEPTS[1]}/>
           <CoreConcept {...CORE_CONCEPTS[2]}/>
