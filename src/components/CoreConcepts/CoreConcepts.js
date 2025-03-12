6c3fefec5b6f087410bab03107d34104ec11438e
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-11 21:30:57 -0600

    changed line item to div

--- src/components/CoreConcepts/CoreConcepts.js
+++ src/components/CoreConcepts/CoreConcepts.js
@@ -1,9 +1,9 @@
 export default function CoreConcept({image, title, description}) {
   return(
-    <li>
+    <div>
       <img src={image} alt={title}/>
       <h3>{title}</h3>
       <p>{description}</p>
-    </li>
+    </div>
   )
 }
