a9010e7d9955ceeb0ee4e98e1a3b69b27a1454d3
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-12 21:04:09 -0600

    made it so the transliterate button turns green when there is text in the textarea

--- src/components/PageContent/PageContent.js
+++ src/components/PageContent/PageContent.js
@@ -1,32 +1,37 @@
+import { useState } from 'react'; // Import useState
 import './PageContent.css';
-import TransliterateButton from '../Buttons/TransliterateButton'
+import TransliterateButton from '../Buttons/TransliterateButton';
 
 export default function PageContent({
   image,
   title,
   description,
   whatIs,
-  transliterator,
-  isActive
-  }) {
-  return(
+  transliterator
+}) {
+  const [text, setText] = useState("");
+  const textareaHasText = text.length > 0;
+
+  return (
     <div>
-      <img src={image} alt={title}/>
+      <img src={image} alt={title} />
       <div>
         <h2>{whatIs}</h2>
         <p>{description}</p>
-     </div>
+      </div>
       <div>
         <h2>{transliterator}</h2>
         <textarea
           className="transliteration-textarea"
           placeholder="Enter text to be transliterated here..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         ></textarea>
         <div>
-          <TransliterateButton isActive={isActive}/>
+          <TransliterateButton isActive={textareaHasText} />
         </div>
         <p className="transliteration-output">output</p>
       </div>
     </div>
-  )
+  );
 }
