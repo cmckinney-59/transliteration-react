9de213161082c26998ddfe2f7c0ea68e3e203ded
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-14 13:59:42 -0600

    added all of the rules

--- src/components/PageContent/PageContent.js
+++ src/components/PageContent/PageContent.js
@@ -74,6 +74,13 @@ export default function PageContent({
     processedText = replaceThWithT(processedText);
     processedText = replaceXWithKs(processedText);
     processedText = capitalizeSubsequentVowels(processedText);
+    processedText = removeDuplicateConsonants(processedText);
+    processedText = replaceNgAndMga(processedText);
+    processedText = replaceNgWithCapitalN(processedText);
+    processedText = addPlusIfConsonant(processedText);
+    processedText = removeAAfterConsonant(processedText);
+    processedText = capitalizeVowel(processedText);
+    processedText = removeDash(processedText);
     return processedText;
   }
 
@@ -102,5 +109,71 @@ export default function PageContent({
     return text.replace(vowelRegex, (firstVowel, subsequentVowels) => {
       return firstVowel + subsequentVowels.toUpperCase();
     });
+  }
+
+  function removeDuplicateConsonants(text) {
+    const consonantRegex = /([bcdfghjklmnpqrstvwxyz])\1+/gi;
+    return text.replace(consonantRegex, '$1');
+  }
+
+  function replaceNgAndMga(text) {
+    return text.replace(/ ng /g, ' naN ').replace(/ mga /g, ' maNa ');
+  }
+
+  function replaceNgWithCapitalN(text) {
+    return text.replace(/ng/g, 'N');
+  }
+
+  function addPlusIfConsonant(text) {
+    const consonantRegex = /[bcdfghjklmnpqrstvwxyz]/i;
+    const punctuationRegex = /[.,!?;-]/;
+    
+      if (!text.trim()) {
+        return '';
+      }
+    
+      return text.split(' ').map(word => {
+        if (!word.trim()) {
+          return word;
+        }
+    
+        let transformedWord = '';
+    
+        for (let i = 0; i < word.length; i++) {
+          transformedWord += word[i];
+    
+          if (word[i] === ' ' || word[i + 1] === ' ') {
+            continue;
+          }
+    
+          if (i < word.length - 1 && consonantRegex.test(word[i]) && consonantRegex.test(word[i + 1])) {
+            transformedWord += '+';
+          }
+    
+          if (i < word.length - 1 && consonantRegex.test(word[i]) && punctuationRegex.test(word[i + 1])) {
+            transformedWord += '+';
+          }
+        }
+
+        if (consonantRegex.test(word[word.length - 1])) {
+          transformedWord += '+';
+        }
+    
+        return transformedWord;
+      }).join(' ').trim();
+    }
+
+  function removeAAfterConsonant(text) {
+    return text.replace(/([BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz])a/g, '$1');
+  }
+
+  function capitalizeVowel(text) {
+    return text.replace(/([.!?])\s*([aeiou])|(^|\s)([aeiou])/gi, (p1, p2, p3, p4) => {
+      return (p1 ? p1 : p3) + (p2 ? p2.toUpperCase() : p4 ? p4.toUpperCase() : '');
+    });
+  }
+
+  function removeDash(text) {
+    return text.replace(/-/g, '');
   }
 }
