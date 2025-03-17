82d7571233414d938207978d9fad5e80869b2d69
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-17 12:44:09 -0600

    replaced left hand nav with dropdown

--- src/components/Buttons/LeftTabButton.jsx
+++
@@ -1,5 +0,0 @@
-export default function LeftTabButton({ children, onClick }) {
-    return (
-        <button onClick={onClick}>{children}</button>
-    )
-}
--- src/components/ContentContainer/ContentContainer.jsx
+++ src/components/ContentContainer/ContentContainer.jsx
@@ -3,34 +3,26 @@ import { useState } from 'react';
 import './ContentContainer.css';
 import {PAGES} from '../../pages.jsx'
 import PageContent from '../PageContent/PageContent.jsx';
-import LeftTabButton from '../Buttons/LeftTabButton.jsx';
 
 export default function ContentContainer() {
   const [ selectedPage, setSelectedPage ] = useState('Home')
 
-  function handleClick(selectedPage) {
-    setSelectedPage (selectedPage);
+  function handleClick(event) {
+    setSelectedPage (event.target.value);
   }
 
   return (
     <section>
       <div>
-      <label htmlFor="options">Choose an option:</label>
-      <select id="options" value={selectedPage} onChange={handleClick}>
-        <option value="">Select...</option>
-        <option value="baybayin">Baybayin</option>
-        <option value="aurebesh">Aurebesh</option>
-        <option value="deseret">Deseret</option>
-      </select>
-      <p>Selected: {selectedPage || "None"}</p>
-    </div>
-      <h1>{selectedPage}</h1>
-      <menu className='sidebar'>
-        <LeftTabButton onClick={() => handleClick('Home')}>Home</LeftTabButton>
-        <LeftTabButton onClick={() => handleClick('Baybayin')}>Baybayin</LeftTabButton>
-        <LeftTabButton onClick={() => handleClick('Aurebesh')}>Aurebesh</LeftTabButton>
-        <LeftTabButton onClick={() => handleClick('Deseret')}>Deseret</LeftTabButton>
-      </menu>
+        <label htmlFor="options">Choose an alphabet:</label>
+        <select id="options" value={selectedPage} onChange={handleClick}>
+          <option value="">Select...</option>
+          <option value="Baybayin">Baybayin</option>
+          <option value="Aurebesh">Aurebesh</option>
+          <option value="Deseret">Deseret</option>
+        </select>
+        <p>Selected: {selectedPage || "None"}</p>
+      </div>
       <div className='page-content'>
         <PageContent {...PAGES[selectedPage]}/>
       </div>
