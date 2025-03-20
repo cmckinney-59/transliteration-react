d59b7baef8d9c621579f8081d57e4f8ca5be0108
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-20 10:51:52 -0600

    added divs to separate the phonetic answers and dialog nav buttons

--- src/components/Dialog/QuestionDialog.jsx
+++ src/components/Dialog/QuestionDialog.jsx
@@ -11,12 +11,15 @@ export default function QuestionDialog({
         <div className="dialog-box">
           <h3>Word Review</h3>
           <p>Does the '{phoneticQuestionChar}' in {enteredText} sound like '{phoneticAnswerChar1}' or '{phoneticAnswerChar2}' or '{phoneticAnswerChar3}'?</p>
-          { phoneticAnswerChar1 !== null ? <button>{phoneticAnswerChar1}</button> : null}
-          { phoneticAnswerChar2 !== null ? <button>{phoneticAnswerChar2}</button> : null}
-          { phoneticAnswerChar3 !== null ? <button>{phoneticAnswerChar3}</button> : null}
-          <button onClick={onClick}>Repace</button>
-          <button onClick={onClick}>Skip</button>
-          <button onClick={onClick}>Close</button>
+          <div className="phonetic-answers">
+            { phoneticAnswerChar1 !== null ? <button>{phoneticAnswerChar1}</button> : null}
+            { phoneticAnswerChar2 !== null ? <button>{phoneticAnswerChar2}</button> : null}
+            { phoneticAnswerChar3 !== null ? <button>{phoneticAnswerChar3}</button> : null}
+          </div>
+          <div className="dialog-nav-buttons">
+            <button onClick={onClick}>Skip</button>
+            <button onClick={onClick}>Close</button>
+          </div>
         </div>
       </div>
     )
