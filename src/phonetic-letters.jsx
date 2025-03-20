715e95c5d72a6e1be0528108241b4b8db6fce24d
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-20 14:41:39 -0600

    added phonetic letters

---
+++ src/phonetic-letters.jsx
@@ -0,0 +1,19 @@
+export const PHONETIC_LETTERS = {
+    C: {
+        phoneticAnswer1: "k",
+        phoneticAnswer2: "s",
+        phoneticAnswer3: "tiy",
+    },
+    CH: {
+        phoneticAnswer1: "k",
+        phoneticAnswer2: "tiy",
+    },
+    J: {
+        phoneticAnswer1: "h",
+        phoneticAnswer2: "diy",
+    },
+    QU: {
+        phoneticAnswer1: "k",
+        phoneticAnswer2: "kuw",
+    },
+}
