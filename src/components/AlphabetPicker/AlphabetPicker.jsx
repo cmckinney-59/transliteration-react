39e167479de6f31afd2382a54e84898c2eb7be68
cmckinney-59 <chazdmckinney@gmail.com>
2025-03-20 15:01:47 -0600

    updated alphabets option picker

--- src/components/AlphabetPicker/AlphabetPicker.jsx
+++ src/components/AlphabetPicker/AlphabetPicker.jsx
@@ -1,14 +1,16 @@
 import './AlphabetPicker.css';
 
 export default function AlphabetPicker({ selectedPage, handleClick}) {
+  const alphabets = ["Baybayin", "Aurebesh", "Deseret"];
+
   return (
     <section>
       <label htmlFor="options">Choose an alphabet: </label>
       <select id="options" value={selectedPage} onChange={handleClick}>
         <option value="">Select...</option>
-        <option value="Baybayin">Baybayin</option>
-        <option value="Aurebesh">Aurebesh</option>
-        <option value="Deseret">Deseret</option>
+        {alphabets.map((alphabet) => (
+          <option key={alphabet} value={alphabet}>{alphabet}</option>
+        ))}
       </select>
     </section>
   );
