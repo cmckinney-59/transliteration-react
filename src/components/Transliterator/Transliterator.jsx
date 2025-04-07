6db8a5128d9a4f4904bb24a46efb05c5c8071976
cmckinney-59 <chazdmckinney@gmail.com>
2025-04-07 15:03:30 -0600

    updated roadmap and add a call to transliterate the output

--- src/components/Transliterator/Transliterator.jsx
+++ src/components/Transliterator/Transliterator.jsx
@@ -1,38 +1,34 @@
-import { useState } from 'react';
-import './Transliterator.css'
+import { useState } from "react";
+import "./Transliterator.css";
 
-import TransliterateButton from '../Buttons/TransliterateButton';
-import QuestionDialog from '../Dialog/QuestionDialog';
-import processBaybayinText from '../Utils/BaybayinTextProcessor'
-import { PHONETIC_LETTERS } from '../../phonetic-letters';
+import TransliterateButton from "../Buttons/TransliterateButton";
+import QuestionDialog from "../Dialog/QuestionDialog";
+import processBaybayinText from "../Utils/BaybayinTextProcessor";
+import { PHONETIC_LETTERS } from "../../phonetic-letters";
 
 function getPhoneticData(text) {
-
   //TODO: Try making switch statement
   if (/[A-Z]/.test(text)) return PHONETIC_LETTERS.none;
   if (/ch/i.test(text)) return PHONETIC_LETTERS.CH;
-  if ((/c/i.test(text)) && (!/ch/i.test(text))) return PHONETIC_LETTERS.C;
+  if (/c/i.test(text) && !/ch/i.test(text)) return PHONETIC_LETTERS.C;
   if (/qu/i.test(text)) return PHONETIC_LETTERS.QU;
   if (/j/i.test(text)) return PHONETIC_LETTERS.J;
-  
-  return PHONETIC_LETTERS.none
+
+  return PHONETIC_LETTERS.none;
 }
 
-export default function Transliterator({
-  title
-  }) {
-
+export default function Transliterator({ title }) {
   const [text, setText] = useState("");
-  const [transliteratedText, setTransliteratedText] = useState("")
+  const [transliteratedText, setTransliteratedText] = useState("");
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [dialogWord, setDialogWord] = useState("");
 
   const phoneticData = getPhoneticData(text);
   const textareaHasText = text.length > 0;
   let showDialog = null;
-  let isBaybayin = title === 'Baybayin';
-  let isAurebesh = title === 'Aurebesh';
-  let isDeseret = title === 'Deseret';
+  let isBaybayin = title === "Baybayin";
+  let isAurebesh = title === "Aurebesh";
+  let isDeseret = title === "Deseret";
 
   const handleTransliterate = () => {
     const wordsArray = text.split(/\s+/);
@@ -40,7 +36,6 @@ export default function Transliterator({
       dict[word] = "";
       return dict;
     }, {});
-
 
     for (const key of Object.keys(wordsDictionary)) {
       const wordIncludesQuestion = /ch|qu|c|j/i.test(key);
@@ -51,26 +46,33 @@ export default function Transliterator({
         setIsDialogOpen(true);
       }
     }
+    runAgainstRules(transliteratedText);
     console.log(wordsDictionary);
-  }
+  };
 
-  function runAgainstRules (text) {
+  function runAgainstRules(text) {
     if (isBaybayin) {
       setTransliteratedText(processBaybayinText(text));
     } else {
       setTransliteratedText(text);
     }
-  };
+  }
 
   const handleCloseDialog = () => {
     setIsDialogOpen(false);
   };
 
   const handlePhoneticAnswerSelected = (selectedAnswer) => {
-    const updatedWord = dialogWord.replace(new RegExp(phoneticData.phoneticQuestion, "gi"), selectedAnswer);
-  
-    const updatedText = text.replace(new RegExp(`\\b${dialogWord}\\b`, "gi"), updatedWord);
-  
+    const updatedWord = dialogWord.replace(
+      new RegExp(phoneticData.phoneticQuestion, "gi"),
+      selectedAnswer
+    );
+
+    const updatedText = text.replace(
+      new RegExp(`\\b${dialogWord}\\b`, "gi"),
+      updatedWord
+    );
+
     setTransliteratedText(updatedText);
   };
 
@@ -79,10 +81,7 @@ export default function Transliterator({
     const wordIncludesCapital = /[A-Z]/.test(dialogWord);
     if (wordIncludesCapital) {
       showDialog = (
-        <QuestionDialog
-          enteredText={dialogWord}
-          close={handleCloseDialog}
-        />
+        <QuestionDialog enteredText={dialogWord} close={handleCloseDialog} />
       );
     } else if (phoneticData) {
       showDialog = (
@@ -102,33 +101,38 @@ export default function Transliterator({
   return (
     <div>
       <h2>{title} Transliterator</h2>
-        <div className="transliteration-container">
-          <textarea
-            className="transliteration-textarea"
-            placeholder="Enter text to be transliterated here..."
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          ></textarea>
-          <p
-            className={`transliteration-output ${
-              textareaHasText ?
-                (isBaybayin ? "baybayin-font" :
-                 isAurebesh ? "aurebesh-font" :
-                 isDeseret ? "deseret-font" :
-                "")
+      <div className="transliteration-container">
+        <textarea
+          className="transliteration-textarea"
+          placeholder="Enter text to be transliterated here..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <p
+          className={`transliteration-output ${
+            textareaHasText
+              ? isBaybayin
+                ? "baybayin-font"
+                : isAurebesh
+                ? "aurebesh-font"
+                : isDeseret
+                ? "deseret-font"
+                : ""
               : ""
-              }`}>
-            {transliteratedText || "Transliterated text..."}
-          </p>
-        </div>
-        <p>{transliteratedText}</p>
-        <div>
-          <TransliterateButton
+          }`}
+        >
+          {transliteratedText || "Transliterated text..."}
+        </p>
+      </div>
+      <p>{transliteratedText}</p>
+      <div>
+        <TransliterateButton
           isActive={textareaHasText}
           onClick={handleTransliterate}
-          text={text}/>
-        </div>
+          text={text}
+        />
+      </div>
       {showDialog}
     </div>
-  )
+  );
 }
