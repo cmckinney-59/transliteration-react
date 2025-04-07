d42d5d58a3f1fb3f3a26ce8af54229b8ff41139f
cmckinney-59 <chazdmckinney@gmail.com>
2025-04-07 16:48:40 -0600

    updated phonetic characters question

--- src/components/Dialog/PhoneticCharictersQuestion.jsx
+++ src/components/Dialog/PhoneticCharictersQuestion.jsx
@@ -1,24 +1,32 @@
 export default function PhoneticCharactersQuestion({
-    enteredText,
-    phoneticQuestionChar,
+  enteredText,
+  phoneticQuestionChar,
+  phoneticAnswerChar1,
+  phoneticAnswerChar2,
+  phoneticAnswerChar3,
+  onSelectPhoneticAnswer,
+}) {
+  const phoneticAnswers = [
     phoneticAnswerChar1,
     phoneticAnswerChar2,
     phoneticAnswerChar3,
-    onSelectPhoneticAnswer
-  }) {
-    const phoneticAnswers = [phoneticAnswerChar1, phoneticAnswerChar2, phoneticAnswerChar3].filter(Boolean);
+  ].reduce((acc, answer) => {
+    if (answer) {
+      acc.push(
+        <button key={answer} onClick={() => onSelectPhoneticAnswer(answer)}>
+          {answer}
+        </button>
+      );
+    }
+    return acc;
+  }, []);
 
-    return (
-      <>
-        <p>What does the '{phoneticQuestionChar}' in '{enteredText}' sound like?</p>
-        <div className="phonetic-answers">
-          {phoneticAnswers.map((answer) => (
-            <button key={answer} onClick={() => onSelectPhoneticAnswer(answer)}>
-                {answer}
-            </button>
-          ))}
-        </div>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <p>
+        What does the '{phoneticQuestionChar}' in '{enteredText}' sound like?
+      </p>
+      <div className="phonetic-answers">{phoneticAnswers}</div>
+    </>
+  );
+}
