890701f7e078e29e989d7483e495e829271b39b9
cmckinney-59 <chazdmckinney@gmail.com>
2025-04-09 16:03:16 -0600

    moved yes/no questions back into propernoun question to simplify

--- src/components/Dialog/ProperNounQuestion.jsx
+++ src/components/Dialog/ProperNounQuestion.jsx
@@ -1,56 +1,60 @@
-import { useState } from 'react';
-import YesNoQuestion from './YesNoQuestion';
+import { useState } from "react";
+import YesNoQuestion from "./YesNoQuestion";
 
-export default function ProperNownQuestion({
-    enteredText,
-    ...props
-  }) {
-    const [isProperNoun, setIsProperNoun] = useState(false);
-    const [notPhonetic, setNotPhonetic] = useState(false);
+export default function ProperNownQuestion({ enteredText, next }) {
+  const [isProperNoun, setIsProperNoun] = useState(false);
+  const [notPhonetic, setNotPhonetic] = useState(false);
 
-    let showProperNounQuestion = null;
-    let showPhoneticQuestion = null;
-    let showEnterPhoneticWord = null;
+  let showProperNounQuestion = null;
+  let showPhoneticQuestion = null;
+  let showEnterPhoneticWord = null;
 
-    const handleIsProperNoun = () => {
+  const handleIsProperNoun = () => {
     setIsProperNoun(true);
-    };
+  };
 
-    const handleNotPhonetic = () => {
+  const handleNotPhonetic = () => {
     setNotPhonetic(true);
-    };
-
+  };
 
   if (!isProperNoun && !notPhonetic) {
-    showProperNounQuestion = 
-      <YesNoQuestion 
-        handleIsProperNoun={handleIsProperNoun} 
-        enteredText={enteredText}
-        {...props} />;
+    showProperNounQuestion = (
+      <>
+        <p>Is "{enteredText}" a proper noun?</p>
+        <div>
+          <button onClick={next}>Yes</button>
+          <button onClick={handleNotPhonetic}>No</button>
+        </div>
+      </>
+    );
   }
 
   if (isProperNoun && !notPhonetic) {
-    showPhoneticQuestion =
-      <YesNoQuestion
-      handleIsProperNoun={handleNotPhonetic}
-      enteredText={enteredText}
-      {...props} />;
+    showPhoneticQuestion = (
+      <>
+        <p>Is "{enteredText}" spelled phonetically?</p>
+        <div>
+          <button onClick={handleIsProperNoun}>Yes</button>
+          <button onClick={next}>No</button>
+        </div>
+      </>
+    );
   }
 
   if (isProperNoun && notPhonetic) {
     showEnterPhoneticWord = (
-        <div>
-            <p>Please spell {enteredText} phonetically.</p>
-            <input />
-        </div>
+      <div>
+        <p>Please spell {enteredText} phonetically.</p>
+        <input />
+      </div>
     );
   }
 
-    return (
-        <>
-            {showProperNounQuestion}
-            {showPhoneticQuestion}
-            {showEnterPhoneticWord}
-        </>
-    )
+  return (
+    <>
+      {showProperNounQuestion}
+      {showPhoneticQuestion}
+      {showEnterPhoneticWord}
+    </>
+  );
 }
