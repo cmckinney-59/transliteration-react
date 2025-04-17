c65bb771ada29df5c53ea008d2605a43f390def8
cmckinney-59 <chazdmckinney@gmail.com>
2025-04-16 22:17:12 -0600

    save as button place holder

--- src/components/Buttons/SaveAsButton.jsx
+++ src/components/Buttons/SaveAsButton.jsx
@@ -2,17 +2,8 @@ export default function saveAsButton({ handleClick }) {
   const fileTypes = ["Word", "Text", "Excel"];
 
   return (
-    <section>
-      <label htmlFor="options">Choose a file type: </label>
-      <select id="options" onChange={handleClick}>
-        <option value=""></option>
-        {fileTypes.map((fileTypes) => (
-          <option key={fileTypes} value={fileTypes}>
-            {fileTypes}
-          </option>
-        ))}
-      </select>
-      <button>Save</button>
-    </section>
+    <>
+      <button>Save as</button>
+    </>
   );
 }
