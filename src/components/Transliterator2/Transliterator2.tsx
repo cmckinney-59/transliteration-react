2edf1efd218c5411282594bd465f9ab3fa52628c
cmckinney-59 <chazdmckinney@gmail.com>
2025-04-29 15:11:49 -0600

    started transliterator from scratch to follow pattern

---
+++ src/components/Transliterator2/Transliterator2.tsx
@@ -0,0 +1,45 @@
+import { useState, JSX } from "react";
+import "./Transliterator2.css";
+
+import TransliterateButton from "../Buttons/TransliterateButton.tsx";
+import React from "react";
+
+interface TransliteratorProps {
+  title: string;
+}
+
+export default function Transliterator({ title }: TransliteratorProps) {
+  const [text, setText] = useState<string>("");
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+  const textareaHasText = text.length > 0;
+
+  let showDialog: JSX.Element | null = null;
+  if (isDialogOpen) {
+    showDialog = <div>dialog</div>;
+  }
+
+  return (
+    <div>
+      <h2>{title} Transliterator 2</h2>
+      <div className="transliteration-container">
+        <textarea
+          className="transliteration-textarea"
+          placeholder="Enter text to be transliterated here..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+      </div>
+      <div>
+        <TransliterateButton
+          isActive={textareaHasText}
+          text={text}
+          onClick={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </div>
+      {showDialog}
+    </div>
+  );
+}
